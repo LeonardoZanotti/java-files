ec019355f1ace8261f49aefdca70973764c9a320
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2021-10-12 16:51:55 -0300

    class06

---
+++ class06/src/class06/Class06.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package class06;
+import java.util.Scanner;  // Import the Scanner class
+
+/**
+ *
+ * @author leonardozanotti
+ */
+public class Class06 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter the text: ");
+        String text = scanner.nextLine();  // Read user input
+        System.out.println("Text length: " + text.length());  // Output user input
+    }
+    
+}
