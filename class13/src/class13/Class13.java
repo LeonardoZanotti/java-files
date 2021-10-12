879a0f55067b933c13e305c0ee801ed892909a6a
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2021-10-12 18:28:41 -0300

    class13

--- class13/src/class13/Class13.java
+++ class13/src/class13/Class13.java
@@ -4,6 +4,8 @@
  */
 package class13;
 
+import java.util.Scanner;
+
 /**
  *
  * @author leonardozanotti
@@ -14,7 +16,19 @@ public class Class13 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter the text:");
+        String text = scanner.nextLine();  // Read user input
+        String formattedText = text.replaceAll("(\\s+|[^a-zA-Z]*)", "").toLowerCase();
+        String output = "Is a palindrome";
+        for (int i = 0; i < formattedText.length() / 2; i++) {
+            if (Character.compare(formattedText.charAt(i), formattedText.charAt(formattedText.length() - 1 - i)) != 0) {
+                output = "Not a palindrome";
+                break;
+            }
+        }
+        System.out.println(output);
+        
     }
     
 }
