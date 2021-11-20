354766c2302c908172f2580645f33f376502f3a0
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2021-11-20 15:20:53 -0300

    mines system

--- Minefield/src/minefield/GUI.java
+++ Minefield/src/minefield/GUI.java
@@ -9,6 +9,7 @@ import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.util.Random;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -22,16 +23,46 @@ public class GUI extends JFrame {
     private int rowSize, colSize;
     private int buttonSize;
     private int spacing;
+    private int bombs;
     
     private int mouseX = -100, mouseY = -100;
     
-    public GUI(int rows, int cols, int rowSize, int colsSize, int buttonSize, int spacing) {
+    private double probability;
+    
+    private int[][] neighbours;
+    private boolean[][] mines;
+    private boolean[][] revealed;
+    private boolean[][] flagged;
+    
+    public GUI(int rows, int cols, int rowSize, int colsSize, int buttonSize, int spacing, int bombs) {
         this.rows = rows;
         this.cols = cols;
         this.rowSize = rowSize;
         this.colSize = colsSize;
         this.buttonSize = buttonSize;
         this.spacing = spacing;
+        this.bombs = bombs;
+        this.probability = (double) bombs / (rows * cols);
+        this.mines = new boolean[rows][cols];
+        this.neighbours = new int[rows][cols];
+        this.revealed = new boolean[rows][cols];
+        this.flagged = new boolean[rows][cols];
+
+        boolean repeating = false;
+        while (bombs > 0) {
+            for (int col = 0; col < cols; col++) {
+                for (int row = 0; row < rows; row++) {
+                    if (bombs > 0 && Math.random() < probability && !mines[row][col]) {
+                        mines[row][col] = true;
+                        bombs--;
+                    } else if (!repeating) {
+                        mines[row][col] = false;
+                    }
+                }
+            }
+            repeating = true;
+        }
+        
         this.setTitle("Minesweeper");
         this.setSize(rowSize, colSize);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -60,6 +91,9 @@ public class GUI extends JFrame {
                     positionY = spacing + (row + 1) * buttonSize;
                     buttonSide = buttonSize - 2 * spacing;
                     g.setColor(Color.GRAY);
+                    if (mines[row][col]) {
+                        g.setColor(Color.YELLOW);
+                    }
                     if (mouseX >= positionX && mouseX <= positionX + buttonSide && mouseY >= positionY - spacing + buttonSide && mouseY <= positionY + 2 * buttonSide) {
                         g.setColor(Color.RED);
                     }
--- Minefield/src/minefield/Minefield.java
+++ Minefield/src/minefield/Minefield.java
@@ -9,9 +9,9 @@ package minefield;
  * @author leonardozanotti
  */
 public class Minefield implements Runnable {
-    GUI gui = new GUI(9, 9, 360, 425, 40, 5);
-//    GUI gui = new GUI(16, 16, 530, 590, 530/16, 4);
-//    GUI gui = new GUI(16, 30, 990, 590, 530/16, 3);
+    GUI gui = new GUI(9, 9, 360, 425, 40, 5, 10);
+//    GUI gui = new GUI(16, 16, 530, 590, 530/16, 4, 40);
+//    GUI gui = new GUI(16, 30, 990, 590, 530/16, 3, 99);
     
     /**
      * @param args the command line arguments
