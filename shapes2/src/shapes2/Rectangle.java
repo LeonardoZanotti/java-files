f698dc4820e9569eae66f704fb1959315a172395
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2022-02-09 14:46:58 -0300

    starting shapes2

---
+++ shapes2/src/shapes2/Rectangle.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package shapes2;
+
+/**
+ *
+ * @author leonardozanotti
+ */
+public class Rectangle {
+    private double side1;
+    private double side2;
+    
+    public Rectangle(double side1, double side2) {
+        this.side1 = side1;
+        this.side2 = side2;
+    }
+}
---
+++ shapes2/src/shapes2/Triangle.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package shapes2;
+
+/**
+ *
+ * @author leonardozanotti
+ */
+public class Triangle {
+    private double height;
+    private double base;
+    
+    public Triangle(double height, double base) {
+        this.height = height;
+        this.base = base;
+    }
+}
