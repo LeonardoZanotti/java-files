f5f21209f092afab638cf764e721378b4f6b18bc
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2022-02-18 08:41:13 -0300

    Usuario

---
+++ Exc2Web2/src/java/models/Usuario.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+/**
+ *
+ * @author leonardozanotti
+ */
+public class Usuario {
+    private String name, password, login;
+    
+    public Usuario() {
+        
+    }
+    
+    public Usuario(String name, String login, String password) {
+        this.name = name;
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+}
