47f4f188eb2933c6e8578023cdb2b9fd4a7333ee
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2022-02-26 13:50:19 -0300

    tarefa 3 de web2

--- Exc2Web2/src/java/models/Usuario.java
+++ Exc2Web2/src/java/models/Usuario.java
@@ -4,21 +4,53 @@
  */
 package models;
 
+import java.io.Serializable;
+
 /**
  *
  * @author leonardozanotti
  */
-public class Usuario {
-    private String name, password, login;
+public class Usuario implements Serializable {
+    private int id;
+    private String login;
+    private String name;
+    private String password;
     
     public Usuario() {
         
     }
     
-    public Usuario(String name, String login, String password) {
-        this.name = name;
+    public Usuario(String login, String name, String password) {
         this.login = login;
+        this.name = name;
         this.password = password;
+    }
+    
+    public Usuario(int id, String login, String name, String password) {
+        this.id = id;
+        this.login = login;
+        this.name = name;
+        this.password = password;
+    }
+    
+    public String toString() {
+        return "id=" + this.id + " / nome=" + this.name;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
     }
 
     public String getName() {
@@ -35,13 +67,5 @@ public class Usuario {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
     }
 }
