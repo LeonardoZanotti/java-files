7fb9353b25fd6c9be952df4aa8f64e5861be81e2
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2022-04-03 16:16:34 -0300

    updated migrate.sql

--- Exc2Web2/src/java/database/migrate.sql
+++ Exc2Web2/src/java/database/migrate.sql
@@ -20,6 +20,19 @@ CREATE TABLE tb_usuario (
     nome_usuario VARCHAR(100) NOT NULL
 );
 
+CREATE TABLE tb_cliente (
+    id_cliente serial PRIMARY KEY,
+    cpf_cliente CHAR(11) NOT NULL,
+    email_cliente VARCHAR(50) NOT NULL,
+    nome_cliente VARCHAR(100) NOT NULL,
+    data_cliente DATE,
+    rua_cliente VARCHAR(100),
+    nr_cliente INTEGER,
+    cep_cliente CHAR(8),
+    id_cidade INTEGER,
+    CONSTRAINT fk_cidade FOREIGN KEY(id_cidade) REFERENCES tb_cidade(id_cidade)
+);
+
 CREATE TABLE tb_estado (
     id_estado serial PRIMARY KEY,
     nome_estado VARCHAR(100),
@@ -42,48 +55,35 @@ VALUES ('joaquim', '12705eee806dbc01eef5d0233cdfa7a8', 'joaquim alfredo');
 INSERT INTO tb_usuario(login_usuario, senha_usuario, nome_usuario)
 VALUES ('novousuario', '82788f72090471a9133b9c0d107d8192', 'novo usuário');
 
-CREATE TABLE tb_cliente (
-    id_cliente serial PRIMARY KEY,
-    cpf_cliente CHAR(11) NOT NULL,
-    email_cliente VARCHAR(50) NOT NULL,
-    nome_cliente VARCHAR(100) NOT NULL,
-    data_cliente DATE,
-    rua_cliente VARCHAR(100),
-    nr_cliente INTEGER,
-    cep_cliente CHAR(8),
-    cidade_cliente VARCHAR(100),
-    uf_cliente CHAR(2)
-);
+INSERT INTO tb_cliente(cpf_cliente, email_cliente, nome_cliente, data_cliente, rua_cliente, nr_cliente, cep_cliente, id_cidade)
+VALUES ('00000000001', 'email1@gmail.com', 'cliente1', '2001-01-01', 'rua 1', 1, '00000001', 4);
 
-INSERT INTO tb_cliente(cpf_cliente, email_cliente, nome_cliente, data_cliente, rua_cliente, nr_cliente, cep_cliente, cidade_cliente, uf_cliente)
-VALUES ('00000000001', 'email1@gmail.com', 'cliente1', '2001-01-01', 'rua 1', 1, '00000001', 'Curitiba 1', 'PR');
+INSERT INTO tb_cliente(cpf_cliente, email_cliente, nome_cliente, data_cliente, rua_cliente, nr_cliente, cep_cliente, id_cidade)
+VALUES ('00000000002', 'email2@gmail.com', 'cliente2', '2002-02-02', 'rua 2', 2, '00000002', 78);
 
-INSERT INTO tb_cliente(cpf_cliente, email_cliente, nome_cliente, data_cliente, rua_cliente, nr_cliente, cep_cliente, cidade_cliente, uf_cliente)
-VALUES ('00000000002', 'email2@gmail.com', 'cliente2', '2002-02-02', 'rua 2', 2, '00000002', 'Curitiba 2', 'PR');
+INSERT INTO tb_cliente(cpf_cliente, email_cliente, nome_cliente, data_cliente, rua_cliente, nr_cliente, cep_cliente, id_cidade)
+VALUES ('00000000003', 'email3@gmail.com', 'cliente3', '2003-03-03', 'rua 3', 3, '00000003', 13);
 
-INSERT INTO tb_cliente(cpf_cliente, email_cliente, nome_cliente, data_cliente, rua_cliente, nr_cliente, cep_cliente, cidade_cliente, uf_cliente)
-VALUES ('00000000003', 'email3@gmail.com', 'cliente3', '2003-03-03', 'rua 3', 3, '00000003', 'Curitiba 3', 'PR');
+INSERT INTO tb_cliente(cpf_cliente, email_cliente, nome_cliente, data_cliente, rua_cliente, nr_cliente, cep_cliente, id_cidade)
+VALUES ('00000000004', 'email4@gmail.com', 'cliente4', '2004-04-04', 'rua 4', 4, '00000004', 512);
 
-INSERT INTO tb_cliente(cpf_cliente, email_cliente, nome_cliente, data_cliente, rua_cliente, nr_cliente, cep_cliente, cidade_cliente, uf_cliente)
-VALUES ('00000000004', 'email4@gmail.com', 'cliente4', '2004-04-04', 'rua 4', 4, '00000004', 'Curitiba 4', 'PR');
+INSERT INTO tb_cliente(cpf_cliente, email_cliente, nome_cliente, data_cliente, rua_cliente, nr_cliente, cep_cliente, id_cidade)
+VALUES ('00000000005', 'email5@gmail.com', 'cliente5', '2005-05-05', 'rua 5', 5, '00000005', 1025);
 
-INSERT INTO tb_cliente(cpf_cliente, email_cliente, nome_cliente, data_cliente, rua_cliente, nr_cliente, cep_cliente, cidade_cliente, uf_cliente)
-VALUES ('00000000005', 'email5@gmail.com', 'cliente5', '2005-05-05', 'rua 5', 5, '00000005', 'Curitiba 5', 'PR');
+INSERT INTO tb_cliente(cpf_cliente, email_cliente, nome_cliente, data_cliente, rua_cliente, nr_cliente, cep_cliente, id_cidade)
+VALUES ('00000000006', 'email6@gmail.com', 'cliente6', '2006-06-06', 'rua 6', 6, '00000006', 870);
 
-INSERT INTO tb_cliente(cpf_cliente, email_cliente, nome_cliente, data_cliente, rua_cliente, nr_cliente, cep_cliente, cidade_cliente, uf_cliente)
-VALUES ('00000000006', 'email6@gmail.com', 'cliente6', '2006-06-06', 'rua 6', 6, '00000006', 'Curitiba 6', 'PR');
+INSERT INTO tb_cliente(cpf_cliente, email_cliente, nome_cliente, data_cliente, rua_cliente, nr_cliente, cep_cliente, id_cidade)
+VALUES ('00000000007', 'email7@gmail.com', 'cliente7', '2007-07-07', 'rua 7', 7, '00000007', 91);
 
-INSERT INTO tb_cliente(cpf_cliente, email_cliente, nome_cliente, data_cliente, rua_cliente, nr_cliente, cep_cliente, cidade_cliente, uf_cliente)
-VALUES ('00000000007', 'email7@gmail.com', 'cliente7', '2007-07-07', 'rua 7', 7, '00000007', 'Curitiba 7', 'PR');
+INSERT INTO tb_cliente(cpf_cliente, email_cliente, nome_cliente, data_cliente, rua_cliente, nr_cliente, cep_cliente, id_cidade)
+VALUES ('00000000008', 'email8@gmail.com', 'cliente8', '2008-08-08', 'rua 8', 8, '00000008', 69);
 
-INSERT INTO tb_cliente(cpf_cliente, email_cliente, nome_cliente, data_cliente, rua_cliente, nr_cliente, cep_cliente, cidade_cliente, uf_cliente)
-VALUES ('00000000008', 'email8@gmail.com', 'cliente8', '2008-08-08', 'rua 8', 8, '00000008', 'Curitiba 88', 'PR');
+INSERT INTO tb_cliente(cpf_cliente, email_cliente, nome_cliente, data_cliente, rua_cliente, nr_cliente, cep_cliente, id_cidade)
+VALUES ('00000000009', 'email9@gmail.com', 'cliente9', '2009-09-09', 'rua 9', 9, '00000009', 425);
 
-INSERT INTO tb_cliente(cpf_cliente, email_cliente, nome_cliente, data_cliente, rua_cliente, nr_cliente, cep_cliente, cidade_cliente, uf_cliente)
-VALUES ('00000000009', 'email9@gmail.com', 'cliente9', '2009-09-09', 'rua 9', 9, '00000009', 'Curitiba 9', 'PR');
-
-INSERT INTO tb_cliente(cpf_cliente, email_cliente, nome_cliente, data_cliente, rua_cliente, nr_cliente, cep_cliente, cidade_cliente, uf_cliente)
-VALUES ('00000000010', 'email10@gmail.com', 'cliente10', '2010-10-10', 'rua 10', 10, '00000010', 'Curitiba 10', 'PR');
+INSERT INTO tb_cliente(cpf_cliente, email_cliente, nome_cliente, data_cliente, rua_cliente, nr_cliente, cep_cliente, id_cidade)
+VALUES ('00000000010', 'email10@gmail.com', 'cliente10', '2010-10-10', 'rua 10', 10, '00000010', 111);
 
 INSERT INTO tb_estado(nome_estado, sigla_estado)
 VALUES ('Acre', 'AC');
