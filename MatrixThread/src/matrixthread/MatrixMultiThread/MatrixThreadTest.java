f9937a8b2bca7d8edf958bb7e33cdbd554315440
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2022-04-06 15:17:57 -0300

    some advancementes in matrix thread

--- MatrixThread/src/matrixthread/MatrixMultiThread/MatrixThreadTest.java
+++ MatrixThread/src/matrixthread/MatrixMultiThread/MatrixThreadTest.java
@@ -1,34 +1,35 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package matrixthread;
+package matrixthread.MatrixMultiThread;
+
+import matrixthread.Matrix;
+
 
 /**
  *
  * @author leonardozanotti
  */
-public class MatrixThread {
+public class MatrixThreadTest {
     private static final int strength = 1500;
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
         long t1 = System.currentTimeMillis();
-        MatrixThread.runSingleThread(strength);
+        MatrixThreadTest.runMultiThread(strength);
         long t2 = System.currentTimeMillis();
         System.out.printf("Time elapsed: %d seconds\n", (t2 - t1)/1000);
     }
     
-    private static Matrix runSingleThread(int length) {
+    private static void runMultiThread(int length) {
         Matrix m1 = new Matrix(length);
         Matrix m2 = new Matrix(length);
+        Matrix m3 = new Matrix(length);
         Matrix.populateMatrix(m1);
         Matrix.populateMatrix(m2);
-        return Matrix.multiply(m1, m2);
-    }
-    
-    private static MatrixMT runMultiThread(int length, int threads) {
-        return new MatrixMT(length);
+        ParallelThreadsCreator.multiply(m1.getMatrix(), m2.getMatrix(), m3.getMatrix());;
     }
 }
+
+
+
---
+++ MatrixThread/src/matrixthread/MatrixTest.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package matrixthread;
+
+/**
+ *
+ * @author leonardozanotti
+ */
+public class MatrixTest {
+    private static final int strength = 1500;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        long t1 = System.currentTimeMillis();
+        MatrixTest.runSingleThread(strength);
+        long t2 = System.currentTimeMillis();
+        System.out.printf("Time elapsed: %d seconds\n", (t2 - t1)/1000);
+    }
+    
+    private static Matrix runSingleThread(int length) {
+        Matrix m1 = new Matrix(length);
+        Matrix m2 = new Matrix(length);
+        Matrix.populateMatrix(m1);
+        Matrix.populateMatrix(m2);
+        return Matrix.multiply(m1, m2);
+    }
+}
