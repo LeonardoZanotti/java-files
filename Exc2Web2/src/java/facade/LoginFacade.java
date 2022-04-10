52b35976dc627814d4191845088dc2969186c8ae
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2022-04-10 15:52:35 -0300

    fixed login

--- Exc2Web2/src/java/facade/LoginFacade.java
+++ Exc2Web2/src/java/facade/LoginFacade.java
@@ -4,10 +4,55 @@
  */
 package facade;
 
+import database.ConnectionFactory;
+import database.DAOException;
+import jakarta.mail.NoSuchProviderException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+import models.Usuario;
+import models.dao.UsuarioDAO;
+
 /**
  *
  * @author leonardozanotti
  */
 public class LoginFacade {
+    public static boolean validLogin(String login, String password, ConnectionFactory factory) throws DAOException, NoSuchAlgorithmException, NoSuchProviderException, java.security.NoSuchProviderException {
+        UsuarioDAO dao = new UsuarioDAO(factory.getConnection());
+        List<Usuario> usuarios = dao.buscarTodos();
+        String hashedPassword = getSecurePassword(password);
+        return usuarios.stream().anyMatch(user -> user.getLogin().equals(login) && user.getPassword().equals(hashedPassword));
+    }
     
+    public static Usuario getUserByLogin(String login, ConnectionFactory factory) throws DAOException {
+        UsuarioDAO dao = new UsuarioDAO(factory.getConnection());
+        Usuario usuario = dao.buscarPeloEmail(login);
+        return usuario;
+    }
+
+    private static String getSecurePassword(String passwordToHash) {
+        String generatedPassword = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+
+            // Add password bytes to digest
+            md.update(passwordToHash.getBytes());
+
+            // Get the hash's bytes
+            byte[] bytes = md.digest();
+
+            // This bytes[] has bytes in decimal format. Convert it to hexadecimal format
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < bytes.length; i++) {
+              sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+
+            // Get complete hashed password in hex format
+            generatedPassword = sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return generatedPassword;
+    }
 }
--- Exc2Web2/src/java/servlets/LoginServlet.java
+++ Exc2Web2/src/java/servlets/LoginServlet.java
@@ -6,22 +6,15 @@ package servlets;
 
 import beans.LoginBean;
 import database.ConnectionFactory;
-import database.DAOException;
-import jakarta.mail.NoSuchProviderException;
+import facade.LoginFacade;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.util.Arrays;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import models.Usuario;
-import models.dao.UsuarioDAO;
 
 /**
  *
@@ -54,9 +47,9 @@ public class LoginServlet extends HttpServlet {
             out.println("</head>");
             out.println("<body>");
             
-            if (validLogin(login, password, factory)) {
+            if (LoginFacade.validLogin(login, password, factory)) {
                 HttpSession session = request.getSession();
-                Usuario user = getUserByLogin(login, factory);
+                Usuario user = LoginFacade.getUserByLogin(login, factory);
                 LoginBean loginBean = new LoginBean(user.getId(), user.getName());
                 session.setAttribute("loginBean", loginBean);
                 response.sendRedirect("./jsp/portal.jsp");
@@ -70,44 +63,6 @@ public class LoginServlet extends HttpServlet {
             out.println("</body>");
             out.println("</html>");
         }
-    }
-    
-    protected boolean validLogin(String login, String password, ConnectionFactory factory) throws DAOException, NoSuchAlgorithmException, NoSuchProviderException, java.security.NoSuchProviderException {
-        UsuarioDAO dao = new UsuarioDAO(factory.getConnection());
-        List<Usuario> usuarios = dao.buscarTodos();
-        String hashedPassword = getSecurePassword(password);
-        return usuarios.stream().anyMatch(user -> user.getLogin().equals(login) && user.getPassword().equals(hashedPassword));
-    }
-    
-    protected Usuario getUserByLogin(String login, ConnectionFactory factory) throws DAOException {
-        UsuarioDAO dao = new UsuarioDAO(factory.getConnection());
-        Usuario usuario = dao.buscarPeloEmail(login);
-        return usuario;
-    }
-
-    private static String getSecurePassword(String passwordToHash) {
-        String generatedPassword = null;
-        try {
-            MessageDigest md = MessageDigest.getInstance("MD5");
-
-            // Add password bytes to digest
-            md.update(passwordToHash.getBytes());
-
-            // Get the hash's bytes
-            byte[] bytes = md.digest();
-
-            // This bytes[] has bytes in decimal format. Convert it to hexadecimal format
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < bytes.length; i++) {
-              sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
-            }
-
-            // Get complete hashed password in hex format
-            generatedPassword = sb.toString();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        return generatedPassword;
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
